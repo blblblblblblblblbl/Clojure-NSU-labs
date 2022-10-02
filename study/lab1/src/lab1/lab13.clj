5c068d647e40dea8df903040d9a5d4f627f5a838
Кирилл Толмачев <stitch00007@gmail.com>
2022-10-02 21:59:14 +0700

    удалил лишний код

--- study/lab1/src/lab1/lab13.clj
+++ study/lab1/src/lab1/lab13.clj
@@ -2,124 +2,24 @@
 ;1.3. Определить функции my-map и my-filter, аналогичные map (для одного списка) и filter, выразив
 ;их через reduce и базовые операции над списками (cons, first, concat и т.п.)
 
-(reduce f nil '(1 2 3))
-(letfn [(func [x] (println x))])
+(defn my-reduce [func acc coll]
+  (if (empty? coll)
+    acc
+    (recur func (func acc (first coll)) (rest coll))))
 
-;идея реализации my map берем функцию которую подают только меняем ее на функцию которая
-;включает в себя эту функцию и объединение в список
 (defn my-map [f coll]
-   (letfn [(func [acc x] (concat acc (list (f x))))]
-     (my-reduce func nil coll)))
+  (letfn [(func [acc x] (conj acc (f x)))]
+    (reverse (my-reduce func nil coll))))
 
-(defn my-map [f coll]   ;через conj
-  (letfn [(func [acc x] (conj acc (f x)))]
-    (reverse (my-reduce func nil coll))))
-(defn my-map [f coll]   ;через conj
-  (letfn [(func [acc x] (conj acc (f x)))]
-    (reverse (my-reduce func nil coll))))
-; вместо reverse [] как работает concat без reverse и на списках виесто векторов
-(my-map inc '(1 2 3 4 5 6))  ;=> (2 3 4 5 6 7)
+(my-map inc '(1 2 3 4 5 6))
 (map inc '(1 2 3 4 5 6))
 
 (my-map (fn [x] (+ 3 x)) '(1 2 3))
 (map (fn [x] (+ 3 x)) '(1 2 3))
-;///////////////////////////////////////самому reduce написать и вместо concat использовать conjoin
-;/////////////////////////////////////////////////////conj к вектору в конец добавляет
-; идея та же только еще включаем с условием
-(concat (list 1 2) (list 3 4))                              ; 1 2 3 4
-(conj (list 1 2) 3)                                         ; 3 1 2
-(conj [1 2] 3)                                              ; 1 2 3
 
 (defn my-filter [f coll]
-  (letfn [(func [acc x] (concat acc (if (f x) (list x))))]
-    (my-reduce func nil coll)))
-
-(defn my-filter [f coll]                                    ;через conj
   (letfn [(func [acc x] (if (f x) (conj acc x) acc))]
     (reverse (my-reduce func nil coll))))
 
 (my-filter even? (list 1 2 3 4 5 6))
 
-(defn my-reduce [f acc coll]                                ;свой reduce
-  (if (empty? (rest coll))
-    (f acc (first coll))
-    (my-reduce f (f acc (first coll)) (rest coll))
-    ))
-; тут нет recur значит будет stack over flow для большой коллекции
-; что с my-reduce и concat
-
-
-(conj '(1 2 3))
-(conj [1 2 3] 4)
-(conj '(1 2 3) 4)
-(cons (list 5 6) (list 2 3 4))
-(concat '(1 2) '(3 4))
-(concat '(1 2) '(3) )
-;/////////////////////////проверка
-(my-reduce + 0 ())
-
-(concat () (list 1 2))
-(take 10 (concat () (list 1 2)))
-(nth (concat () (list 1 2)) 3)
-(cons (first (list 1 2)) (concat (rest (list 1 2)) (list 3 4)))
-
-(chunk-cons (chunk (chunk-buffer 32)) (seq (range 42)))
-
-()
-(lazy-seq
-  (let [s (seq x)]
-    (if s
-      (cons (first s) (concat (rest s) y))
-      y)))
-
-(chunk-buffer 32)
-(chunk (chunk-buffer 32))
-(seq (range 42))
-
-(chunk-rest
-  (chunk-cons (chunk (chunk-buffer 32))
-              (seq (range 42))))
-
-
-(empty? (rest (list 1)))
-(+ nil 1)
-(reduce + (list 1 2 3))
-(my-reduce + 0 (list 1 2 3))
-
-(my-reduce + 0 '())
-(+ 1 nil)
-
-(my-filter even? (list 1 2 3 4 5 6 ))
-(filter even? (list 1 2 3 4 5 6 ))
-
-(my-filter int? (list 1 2.5 "foo" 7))
-(filter int? (list 1 2.5 "foo" 7))
-
-(my-filter pos? (list -1 5 42 0 -100 3))
-(filter pos? (list -1 5 42 0 -100 3))
-;
-;///////////////////////////////////////////////////////////////////////////////////////
-;просто записи, читать не надо
-;///////////////////////////////////////////////////////////////////////////////////////
-(defn my-reduce [f acc coll]
-  (if (empty? coll)
-    acc
-    (f acc (my-reduce f (first coll) (rest coll)))
-    ))
-
-(defn my-reduce [f acc coll]
-  (if (empty? coll)
-    acc
-    (f (my-reduce f (first coll) (rest coll)) acc)
-    ))
-
-(defn my-reduce [f acc coll]
-  (if (empty? (rest coll))
-    (f acc (first coll))
-    (f acc (my-reduce f (first coll) (rest coll)))
-    ))
-
-(defn my-reduce [func acc coll]
-  (if (empty? coll)
-    acc
-    (recur func (func acc (first coll)) (rest coll))))
--- study/lab1/src/lab1/lab14.clj
+++ study/lab1/src/lab1/lab14.clj
@@ -1,130 +1,13 @@
 (ns lab1.lab14)
 
-(defn func [symbols n]                                      ;требуемая к реализации функция
-  (if (not= n 0)                                            ;condition
-    (my-loop (list(list)) symbols n)                                   ;true
-    ()                                                      ;false
-    ))
-
-(defn func [symbols n]                                      ;требуемая к реализации функция
-  (if (not= n 0)                                            ;condition
-    (first (nth (iterate (fn [mass] (list ( next-gen-comb (first mass) symbols) (dec (last mass)))) (list (list(list)) n)) n))     ;true
-    ()                                                      ;false
-    ))
-; как это работает мы должны подействовать next-gen-comb n раз на свой же вывод
-; поэтому создаем конструкцию mass = (list comb n)
-; делаем iterate который создаст нам список из которого нужно взять nth элемент чтобы получить вывод на определенной итерации
-;функция для iterate берет mass на комб действует next-gen-comb а на n dec
-
-(defn func [symbols n]                                      ;требуемая к реализации функция
-  (if (not= n 0)                                            ;condition
-    (nth (iterate (fn [combs] (next-gen-comb combs symbols) ) (list(list))) n)     ;true
-    ()                                                      ;false
-    ))
-
-(defn func [symbols n]                                      ;требуемая к реализации функция
-  (nth (iterate (fn [combs] (next-gen-comb combs symbols) ) (list(list))) n))
-; а зачем нам вообще тогда n, он не нужен нам ,убрал его
-
-(nth (iterate (fn [mass] (list ( next-gen-comb (first mass) symbols) (dec (last mass)))) (list (list(list)) n)) n)
-; еще меньше стал код
-
-
-(defn func [symbols n]                                      ;требуемая к реализации функция
-  (nth (iterate (fn [combs] (
-                              ;////////next-gen-comb
-                             (fn [combinations symbols]  ;использует функцию add-symbol просто для всех слов а не для одного и выдает список новых комбинаций
-                               (letfn [
-                                       (add-symbol-map [comb] (add-symbol comb symbols))
-
-                                       (add-symbol [combination symbols]             ; эта функция дописывает букву в начало слова и возвращает список слов например если подали (list  "d") (list "a") ;=> (("a" "d") ("b" "d") ("c" "d"))
-                                         ;///////////////////////////////////////////mapcat самому
-                                         (letfn [(fltr [symb] (not= (first combination) symb))]
-                                           (letfn [(add-map [filtred-symbs] (cons filtred-symbs combination))]
-                                             (map add-map (filter fltr symbols)))
-                                           )                                                       ;переделано на map
-                                         )
-
-                                       ( my-mapcat [f coll]
-                                         (letfn [(func [acc x] (concat acc (f x)))]
-                                           (reduce func nil coll)))
-                                       ]
-                                 (my-mapcat add-symbol-map combinations)        ;замена mapcat
-                                 ))
-                              ;///////next-gen-comb
-                              combs symbols) ) (list(list))) n))
-
-
-(defn my-loop [combinations symbols n]         ;данная функция нужна нам вместо цикла чтобы проделать шаг n раз и довести длину комбинация до n
-  (if (= n 1)
-    (next-gen-comb combinations symbols)
-    (my-loop (next-gen-comb combinations symbols) symbols (dec n))))
-
-(defn next-gen-comb [combinations symbols]  ;использует функцию add-symbol просто для всех слов а не для одного и выдает список новых комбинаций
-  (letfn [(add-symbol-map [comb] (add-symbol comb symbols))]
-    (mapcat add-symbol-map combinations)
-    ))                  ;переделано для mapcat ввел функцию которая с помощью map будет без рекурсии для всех комбинаций работать
-
-(defn next-gen-comb [combinations symbols]  ;использует функцию add-symbol просто для всех слов а не для одного и выдает список новых комбинаций
-  (letfn [(add-symbol-map [comb] (add-symbol comb symbols))]
-    (apply concat (map add-symbol-map combinations))        ;замена mapcat
-    ))
-
 (defn my-mapcat [f coll]
   (letfn [(func [acc x] (concat acc (f x)))]
     (reduce func nil coll)))
 
-(defn next-gen-comb [combinations symbols]  ;использует функцию add-symbol просто для всех слов а не для одного и выдает список новых комбинаций
-  (letfn [(add-symbol-map [comb] (add-symbol comb symbols))]
-    (my-mapcat add-symbol-map combinations)        ;замена mapcat
-    ))
+(defn func [symbols n]
+  (nth (iterate (fn [combs]
+    (letfn [(add-symbol [combination symbols] (map #(cons % combination) (filter #(not= (first combination) %) symbols) ))]
+      (mapcat #(add-symbol % symbols) combs))) (list(list))) n))
 
-(defn add-symbol [combination symbols]             ; эта функция дописывает букву в начало слова и возвращает список слов например если подали (list  "d") (list "a") ;=> (("a" "d") ("b" "d") ("c" "d"))
-;///////////////////////////////////////////mapcat самому
-  (letfn [(fltr [symb] (not= (first combination) symb))]
-    (letfn [(add-map [filtred-symbs] (cons filtred-symbs combination))]
-      (map add-map (filter fltr symbols)))
-    )                                                       ;переделано на map
-  )
-
-(my-mapcat reverse (list (list 1 2 3) (list 4 5 6)))     ;=> (3 2 1 6 5 4)
-
-                                                                                                            ;
 
 (func (list "a" "b" "c" "d") 3)
-;//////////////////////////////////////////////////////////////////////////////////////
-;все, дальше просто мои записи проверочные, когда прогу писал
-;///////////////////////////////////////////////////////////////////////////////
-
-
-
-(take 3 (iterate inc 5))        ;=> (5 6 7)
-(take 3 (iterate  (fn [a] a) 5));=> (5 5 5)
-(take 3 (iterate  (fn [a] 4) 5)) ;=> (5 4 4)
-;то есть видно что iterate сначала ставит x на первое место, а потом уже начиная со второго элемента действует на предыдущий
-(take 3 (iterate dec 3))     ;=> (3 2 1)
-(nth (iterate dec 3) 2)
-(take n (iterate dec n))
-
-
-
-
-(mapcat  (list 1 2 3))
-(map reverse (list (list 1 2 3) (list 4 5 6)))              ;=> ((3 2 1) (6 5 4))
-(apply concat (map reverse (list (list 1 2 3) (list 4 5 6)))) ;=> (3 2 1 6 5 4)
-(mapcat reverse (list (list 1 2 3) (list 4 5 6)))           ;=> (3 2 1 6 5 4)
-(map add-symbol combinations symbols)
-(map  + (list 1 2 3) (1))
-(letfn [(add-symbol-map [comb] (add-symbol comb symbols))])
-
-(next-gen-comb (list (list "a" "b") (list "a" "b")) (list "c" "d"))
-
-(add-symbol (list  "d") (list "a" "b" "c" "d"))
-
-(defn aaa [comb symbs]
-  (letfn [(fltr [symb] (not= (first comb) symb))]
-    (filter fltr symbs)
-    ))
-(aaa (list "a" "b") (list "a" "b" "c"))
-
-
